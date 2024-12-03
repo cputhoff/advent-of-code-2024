e474242df357a74d7ff3061518119190915f9b58
Christine Puthoff <christine@Christines-MBP.localdomain>
2024-12-03 16:40:53 -0600

    day 3 part 2

--- day-3/day-3.py
+++ day-3/day-3.py
@@ -1,11 +1,27 @@
 import re
 
 def extract_mul_from_input(input):
-    mul_list =[]
+    mul_list = []
     with open(input, 'r') as f:
         content = f.read()
-        mul_list = re.findall(r"mul\(\d+,\d+\)", content)
+    mul_list = re.findall(r"mul\(\d+,\d+\)", content)
     return mul_list
+
+def extract_mul_with_conditional_from_input(input):
+    mul_list = []
+    do_active = True
+    with open(input, 'r') as f:
+        content = f.read()
+    instructions = re.finditer(r"(mul\(\d+,\d+\))|(do\(\))|(don't\(\))", content)
+    for element in instructions:
+        if element.group(0) == "don't()":
+            do_active = False
+        elif element.group(0) == "do()":
+            do_active = True
+        else:
+            if do_active:
+                mul_list.append(element.group(0))
+    return mul_list   
 
 def execute_mul(mul_string):
     numbers = re.findall(r"\d+", mul_string)
@@ -19,4 +35,6 @@ def sum_execute_mul(mul_list):
 
 input = "input.txt"
 mul_list = extract_mul_from_input(input)
-print(sum_execute_mul(mul_list))
+print("part 1: " + str(sum_execute_mul(mul_list)))
+mul_with_conditional = extract_mul_with_conditional_from_input(input)
+print("part 2: " + str(sum_execute_mul(mul_with_conditional)))
